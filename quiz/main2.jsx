b921cbd9fc73f4be93c3bdc53b4699006cfe3501
unknown <mouhamedaminkraiem09@gmail.com>
2023-06-14 00:03:31 -1100

    simple commit

---
+++ quiz/main2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index2.css'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root2')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ src/intro.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+function Intro() {
+    return (
+        <div className='top-cont'>
+            <div className='container'>
+
+                <h1>Quizzical</h1>
+                <h5>A game made By Houssain and Mohamed</h5>
+                <a href='./quiz/quiz.html'><button type="button" className="btn btn-primary btn-lg">Start Quiz</button></a>
+        </div>
+                
+
+
+
+        </div>
+
+    
+    
+    )
+
+  
+}
+
+export default Intro
